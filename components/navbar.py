318d76f18387fe78ef71c659e17948257a0f21fc
Belluom1n1 <63367279+Belluom1n1@users.noreply.github.com>
2023-08-11 15:20:39 -0700

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -7,8 +7,6 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
-import dash_bootstrap_components.icons
-import dash_extensions as de
 
 BRAND = '/assets/compassMech.ico'
 myBrand = html.Div(
