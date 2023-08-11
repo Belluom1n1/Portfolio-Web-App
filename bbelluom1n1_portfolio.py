4449e0f710bacabac6c97bacde2fe0af1854229a
Belluom1n1 <63367279+Belluom1n1@users.noreply.github.com>
2023-08-11 15:04:08 -0700

    add file

---
+++ bbelluom1n1_portfolio.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# + -------------------------------- +
+# + ~> Brian Belluomini              +
+# + ~> Portfolio Web-App1            +
+# +                                  +
+# + ~> 'python run app.py' & visit   +
+# +     https://127.0.0.1:8050       +
+# + -------------------------------- +
+
+# + ---- library imports ---- +
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash
+
+# + ---- components & assets ---- +
+from components import navbar
+
+# + ---- css // styles // declarations ---- +
+FA621 = "https://use.fontawesome.com/releases/v6.2.1/css/all.css"
+TYPOGRAPHY = "/assets/typography.css"
+
+# import Navbar
+nav = navbar.navbar()
+
+# + ---- initialize app ---- +
+app = Dash(__name__,
+           use_pages= True,
+           external_stylesheets= [dbc.themes.FLATLY, FA621, TYPOGRAPHY],
+           suppress_callback_exceptions= True,
+           meta_tags= [{'name': 'viewport',
+                        'content': 'width= device-width, initial-scale=1',
+                        'charSet': 'utf-8'}])
+
+
+# + ---- layout ---- +
+''' Call components/pages and wrap them in a dbc.Container '''
+app.layout = dbc.Container(
+    [nav, dash.page_container], fluid= True,
+)
+
+
+# + ---- run server ---- +
+# Run server with debug developer mode
+if __name__ == '__main__':
+    app.run_server(port= 5500, debug= True)
+
